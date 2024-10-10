dc73efcb44b8e3a03be60f1a0de5edcaf9ac5baa
NataliaMuratova1979 <mno@mail.ru>
2024-10-10 21:09:12 +0300

    Первый коммит в ветку review

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,19 +1,32 @@
 import arrow from 'src/images/arrow.svg';
-
 import styles from './ArrowButton.module.scss';
+import clsx from 'clsx';
 
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+type ArrowButtonProps = {
+	onClick: (data: boolean) => void; // Обработчик клика
+	isOpen: boolean; // Состояние кнопки (открыто/закрыто)
+};
+
+export const ArrowButton = ({ onClick, isOpen }: ArrowButtonProps) => {
 	return (
-		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={clsx(styles.container, isOpen && styles.container_open)}
+			onClick={() => onClick(!isOpen)} // Добавляем обработчик клика
+			onKeyPress={(e) => {
+				if (e.key === 'Enter') onClick(!isOpen); // Передаем аргумент
+			}}>
+			<img
+				src={arrow}
+				alt='иконка стрелочки'
+				className={styles.arrow}
+				style={{ transform: isOpen ? 'rotate(180deg)' : 'rotate(0deg)' }}
+			/>
 		</div>
 	);
 };
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,13 +1,56 @@
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
-
+import { useState, useRef } from 'react';
 import styles from './ArticleParamsForm.module.scss';
+import clsx from 'clsx';
 
-export const ArticleParamsForm = () => {
+//import { RadioGroup } from 'components/radio-group';
+
+import {
+	ArticleStateType,
+	//fontSizeOptions,
+	//OptionType,
+} from 'src/constants/articleProps';
+import { useOutsideClickClose } from '../select/hooks/useOutsideClickClose';
+
+type ArticleParamsFormProps = {
+	setCurrentArticleState: (param: ArticleStateType) => void;
+	currentArticleState: ArticleStateType;
+};
+
+export const ArticleParamsForm = ({}: //currentArticleState,
+//setCurrentArticleState,
+ArticleParamsFormProps) => {
+	const [isOpen, setIsOpen] = useState<boolean>(false); // начальное состояние переменной open будет false
+
+	const rootRef = useRef<HTMLDivElement>(null); // создаем реф для контейнера
+
+	//const [selectArticleState, setSelectArticleState] =
+	//useState<ArticleStateType>(currentArticleState);
+
+	//const handleChange = (key: keyof ArticleStateType, value: OptionType) => {
+	//	setSelectArticleState({ ...selectArticleState, [key]: value });
+	//};
+
+	//const handleArrowButtonClick = () => {
+	//	setIsOpen((prevState) => !prevState); // переключаем состояние
+	//		console.log('Arrow button clicked in ArticleParamsForm!'); // Сообщение в консоли
+	//	};
+
+	useOutsideClickClose({
+		isOpen,
+		rootRef,
+		onClose: () => setIsOpen(false),
+		onChange: setIsOpen,
+		event: 'mousedown',
+	});
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
+			<ArrowButton onClick={setIsOpen} isOpen={isOpen} />
+
+			<aside
+				className={clsx(styles.container, isOpen && styles.container_open)}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
